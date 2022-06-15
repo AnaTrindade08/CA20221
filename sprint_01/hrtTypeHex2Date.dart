c07b8969bd08e7b673aa3e9f12d99114c79357b0
elielprado <eliel.resende2010@hotmail.com>
2022-06-15 00:41:34 -0300

    Conversor de Hex para Data concluido, falta o tratamento de exceções e testes

--- sprint_01/hrtTypeHex2Date.dart
+++ sprint_01/hrtTypeHex2Date.dart
@@ -6,39 +6,20 @@ import 'dart:core';
 import 'package:intl/intl.dart';
 
 void main() {
-  print('Hello World');
-  String hex = '62A809';
+  String hex = '0F 0A 7a';
   hrtTypeHex2Date(hex);
 }
 
 hrtTypeHex2Date(String strHex) {
-  //this function converts a hexadecimal string to a date
-  //the date is represented by: DDMMYY-1900
-  print(strHex.length);
-  if (strHex.length != 6) {
+  if (strHex.length < 6) {
     print("String Invalida");
     return -1;
   }
-  //convert the string to a list of bytes
-  List<int> bytes = strHex.codeUnits;
-  print(bytes.length);
-  //convert the list of bytes to a list of integers
-  List<int> ints = new List<int>.empty(growable: true);
-  for (int i = 0; i < bytes.length; i++) {
-    ints.add(bytes[i]);
-  }
-  //convert the list of integers to a list of strings
-  List<String> strs = new List<String>.empty(growable: true);
-  for (int i = 0; i < ints.length; i++) {
-    strs.add(ints[i].toRadixString(16));
-  }
-  //convert the list of strings to a string
-  String str = strs.join();
-
-  //convert the string to a date
-  DateTime date = DateTime.parse(str);
-  //convert the date to a string
-  String strDate = DateFormat('ddMMyy').format(date);
-  //print the date
-  print(strDate);
+  //copy strHex to a vector of chars
+  var strHexVector = strHex.split(' ');
+  int day = int.parse(strHexVector.elementAt(0), radix: 16);
+  int month = int.parse(strHexVector.elementAt(1), radix: 16);
+  int year = 1900 + (int.parse(strHexVector.elementAt(2), radix: 16));
+  //print date in format dd/mm/yyyy
+  print(day.toString() + '/' + month.toString() + '/' + year.toString());
 }
